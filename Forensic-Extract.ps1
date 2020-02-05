a11ea64c8556922128d0dc2336c5a67de86c7419
Eidorian <32389491+RedFou52@users.noreply.github.com>
2020-02-05 13:47:32 +0100

    Update Forensic-Extract.ps1

--- Forensic-Extract.ps1
+++ Forensic-Extract.ps1
@@ -21,6 +21,7 @@ Write-Host "
                                                                                 "
 
 $ErrorActionPreference= 'silentlycontinue' #don't display errors
+#$ErrorActionPreference='continue'
 $path = Read-Host "Chemin absolu vers le programme nc.exe [C:\users\toto\Tools] : "
 $ipServ = Read-Host "Entrez l'adresse IP du serveur Netcat : "
 $portServ = Read-host "Entrez le port d'écoute du serveur Netcat [Défaut:4444] : "
@@ -416,37 +417,389 @@ Write-Host "Account Usage ..."
 (echo "# Account Usage #"| .\nc.exe $ipServ $portServ -w $timer) 
 
 # Account Lockouts
+$Error.Clear()
+$accLock =Get-EventLog Security -InstanceId 4740
+if(!$Error){
+    (echo "# Account Lockouts" $accLock | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Account Lockouts "
+}else{
+    Write-Host "[-] Account Lockouts "
+}
+
 # User Add to privileged Group
+$Error.Clear()
+$userAddPriv =Get-EventLog Security -InstanceId 4732
+if(!$Error){
+    (echo "# User Add to privileged Group" $userAddPriv | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] User Add to privileged Group "
+}else{
+    Write-Host "[-] User Add to privileged Group "
+}
+
 # Security-enabled local group Modification
+$Error.Clear()
+$securityEnableLocal =Get-EventLog Security -InstanceId 4735
+if(!$Error){
+    (echo "# Security-enabled local group Modification" $securityEnableLocal | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Security-enabled local group Modification "
+}else{
+    Write-Host "[-] Security-enabled local group Modification "
+}
+
 # Successful user Account Login
+$Error.Clear()
+$successUserAccLog =Get-EventLog Security -InstanceId 4624
+if(!$Error){
+    (echo "# Successful user Account Login" $successUserAccLog | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Successful user Account Login "
+}else{
+    Write-Host "[-] Successful user Account Login "
+}
+
 # Failed user Account login
+$Error.Clear()
+$failUserAccLog =Get-EventLog Security -InstanceId 4625
+if(!$Error){
+    (echo "# Failed user Account login" $failUserAccLog | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Failed user Account login "
+}else{
+    Write-Host "[-] Failed user Account login "
+}
+
 # Account Login with Explicit Credentials
+$Error.Clear()
+$accLogExpli =Get-EventLog Security -InstanceId 4648
+if(!$Error){
+    (echo "# Account Login with Explicit Credentials" $accLogExpli | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Account Login with Explicit Credentials "
+}else{
+    Write-Host "[-] Account Login with Explicit Credentials "
+}
+
 # TGT Request
+$Error.Clear()
+$TGT =Get-EventLog Security -InstanceId 4768
+if(!$Error){
+    (echo "# TGT Request" $TGT | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] TGT Request "
+}else{
+    Write-Host "[-] TGT Request "
+}
+
 # TGS Request
-# Sepial Privileges assigned to new logon
+$Error.Clear()
+$TGS =Get-EventLog Security -InstanceId 4769
+if(!$Error){
+    (echo "# TGS Request" $TGS | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] TGS Request "
+}else{
+    Write-Host "[-] TGS Request "
+}
+
+# Special Privileges assigned to new logon
+$Error.Clear()
+$specialPriv =Get-EventLog Security -InstanceId 4672
+if(!$Error){
+    (echo "# Special Privileges assigned to new logon" $specialPriv | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Special Privileges assigned to new logon "
+}else{
+    Write-Host "[-] Special Privileges assigned to new logon "
+}
 
 ####################  -- Kernel driver signing -- #################### 
 Write-Host "Kernel driver signing ..."
 (echo "# Kernel driver signing #"| .\nc.exe $ipServ $portServ -w $timer)
+
 # Detected an invalid image hash of a file
-# Code integrity check
+$Error.Clear()
+$detectedHash =Get-EventLog Security -InstanceId 5038
+if(!$Error){
+    (echo "# Detected an invalid image hash of a file" $detectedHash | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Detected an invalid image hash of a file "
+}else{
+    Write-Host "[-] Detected an invalid image hash of a file "
+}
+
+# Code integrity check 
+$Error.Clear()
+$codeCheck =Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-CodeIntegrity/Operational"; ID="3001","3002","3003","3004","3010","3023"}
+if(!$Error){
+    (echo "# Code integrity check " $codeCheck | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Code integrity check  "
+}else{
+    Write-Host "[-] Code integrity check  "
+}
 
 ####################  -- Windows defender activities -- #################### 
 Write-Host "Windows defender activities ..."
 (echo "# Windows defender activities #"| .\nc.exe $ipServ $portServ -w $timer)
-# Scan Failed
+
+# Scan Failed   
+$Error.Clear()
+$scanFail =Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Windows Defender/Operational"; ID="1005"}
+if(!$Error){
+    (echo "# Scan Failed    " $scanFail | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Scan Failed     "
+}else{
+    Write-Host "[-] Scan Failed     "
+}
+
 # Detected Malware
+$Error.Clear()
+$detectMalware =Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Windows Defender/Operational"; ID="1006"}
+if(!$Error){
+    (echo "# Detected Malware    " $detectMalware | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Detected Malware  "
+}else{
+    Write-Host "[-] Detected Malware   "
+}
+
 # Failed to update signatures
+$Error.Clear()
+$failSign =Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Windows Defender/Operational"; ID="2001"}
+if(!$Error){
+    (echo "# Failed to update signatures " $failSign | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Failed to update signatures "
+}else{
+    Write-Host "[-] Failed to update signatures "
+}
+
 # Failed to update engine
+$Error.Clear()
+$failUp =Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Windows Defender/Operational"; ID="2003"}
+if(!$Error){
+    (echo "# Failed to update engine " $failUp | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Failed to update engine "
+}else{
+    Write-Host "[-] Failed to update engine "
+}
+
 # Reverting to last known good set of signatures
+$Error.Clear()
+$revertingSign =Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Windows Defender/Operational"; ID="2004"}
+if(!$Error){
+    (echo "# Reverting to last known good set of signatures " $revertingSign | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Reverting to last known good set of signatures "
+}else{
+    Write-Host "[-] Reverting to last known good set of signatures "
+}
+
 # Real-Time Protection failed
+$Error.Clear()
+$realTimeFail =Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Windows Defender/Operational"; ID="3002"}
+if(!$Error){
+    (echo "# Real-Time Protection failed " $realTimeFail | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Real-Time Protection failed "
+}else{
+    Write-Host "[-] Real-Time Protection failed "
+}
+
 # Unexpected Error
+$Error.Clear()
+$unexpectedError =Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-Windows Defender/Operational"; ID="5008"}
+if(!$Error){
+    (echo "# Unexpected Error " $unexpectedError | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Unexpected Error "
+}else{
+    Write-Host "[-] Unexpected Error "
+}
+
+####################  -- Remote Desktop Logon Detection -- #################### 
+Write-Host " Remote Desktop Logon Detection ..."
+(echo "#  Remote Desktop Logon Detection #"| .\nc.exe $ipServ $portServ -w $timer)
+
+# Sucessful User Account Login   (LogonType=10)
+$Error.Clear()
+$successUserAccLogType =Get-EventLog Security -InstanceId 4624
+if(!$Error){
+    (echo "# Sucessful User Account Login   (LogonType=10) " $successUserAccLogType | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Sucessful User Account Login   (LogonType=10) "
+}else{
+    Write-Host "[-] Sucessful User Account Login   (LogonType=10) "
+}
+
+# Sucessful User Account Logoff (LogonType=10)
+$Error.Clear()
+$successUserAccLogOffType =Get-EventLog Security -InstanceId 4634
+if(!$Error){
+    (echo "#Sucessful User Account Logoff (LogonType=10) " $successUserAccLogOffType | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Sucessful User Account Logoff (LogonType=10) "
+}else{
+    Write-Host "[-] Sucessful User Account Logoff (LogonType=10) "
+}
+
+# RDP: Ouverture de session réussie
+$Error.Clear()
+$RDPopenOK =Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-LocalSessionManager/Operational"; ID="21"}
+if(!$Error){
+    (echo "#RDP: Ouverture de session réussie " $RDPopenOK | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] RDP: Ouverture de session réussie "
+}else{
+    Write-Host "[-] RDP: Ouverture de session réussie "
+}
+
+# RDP: Réouverture de session réussie
+$Error.Clear()
+$RDPreopenOK =Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-LocalSessionManager/Operational"; ID="25"}
+if(!$Error){
+    (echo "#RDP: Réouverture de session réussie " $RDPreopenOK | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] RDP: Réouverture de session réussie "
+}else{
+    Write-Host "[-] RDP: Réouverture de session réussie "
+}
+
+# RDP: Initialisation de Plugin
+$Error.Clear()
+$RDPinitPlugin=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-LocalSessionManager/Operational"; ID="32"}
+if(!$Error){
+    (echo "#RDP: Initialisation de Plugin " $RDPinitPlugin | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] RDP: Initialisation de Plugin "
+}else{
+    Write-Host "[-] RDP: Initialisation de Plugin "
+}
+
+# RDP: Démarrage de session
+$Error.Clear()
+$RDPstartSession=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-LocalSessionManager/Operational"; ID="57","58"}
+if(!$Error){
+    (echo "#RDP: Démarrage de session " $RDPstartSession | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] RDP: Démarrage de session "
+}else{
+    Write-Host "[-] RDP: Démarrage de session "
+}
+
+# RDP: Ouverture de connexion
+$Error.Clear()
+$RDPopenCo=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational"; ID="261"}
+if(!$Error){
+    (echo "#RDP: Ouverture de connexion " $RDPopenCo | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] RDP: Ouverture de connexion "
+}else{
+    Write-Host "[-] RDP: Ouverture de connexion "
+}
+
+# RDP: Authentification Réussie/Echouée
+$Error.Clear()
+$RDPauthOKfail=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational"; ID="1148","1149"}
+if(!$Error){
+    (echo "# RDP: Authentification Réussie/Echouée " $RDPauthOKfail | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+]  RDP: Authentification Réussie/Echouée "
+}else{
+    Write-Host "[-]  RDP: Authentification Réussie/Echouée "
+}
+
+# RDP: Session Initiée
+$Error.Clear()
+$RDPsessionInit=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TerminalServices-RemoteConnectionManager/Operational"; ID="1146"}
+if(!$Error){
+    (echo "# RDP: Session Initiée " $RDPsessionInit | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+]  RDP: Session Initiée "
+}else{
+    Write-Host "[-]  RDP: Session Initiée "
+}
+
 
 ####################  -- Scheduled task -- #################### 
 Write-Host "Scheduled task ..."
 (echo "# Scheduled task #"| .\nc.exe $ipServ $portServ -w $timer)
-# Démarrage de la tâche
-# Enregistrement de la tâche
-# Lancement de la tâche 
-# Fin de la tâche
+$datenow=Get-Date -Format "dd/MM/yyyy"
 
+# Démarrage de la tâche
+$Error.Clear()
+$STstartTask=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TaskScheduler/Operational"; ID="100"; StartTime=$datenow}
+if(!$Error){
+    (echo "#Démarrage de la tâche " $STstartTask | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+]  Démarrage de la tâche "
+}else{
+    Write-Host "[-]  Démarrage de la tâche "
+}
+
+# Enregistrement de la tâche
+$Error.Clear()
+$STsaveTask=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TaskScheduler/Operational"; ID="106"}
+if(!$Error){
+    (echo "#Enregistrement de la tâche " $STsaveTask | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+]  Enregistrement de la tâche "
+}else{
+    Write-Host "[-]  Enregistrement de la tâche "
+}
+
+# Lancement de la tâche 
+$Error.Clear()
+$STlaunchTask=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TaskScheduler/Operational"; ID="107","108","109","110","129"; StartTime=$datenow}
+if(!$Error){
+    (echo "#Lancement de la tâche " $STlaunchTask | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+]  Lancement de la tâche "
+}else{
+    Write-Host "[-]  Lancement de la tâche "
+}
+
+# Fin de la tâche
+$Error.Clear()
+$STendTask=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-TaskScheduler/Operational"; ID="102"; StartTime=$datenow}
+if(!$Error){
+    (echo "#Fin de la tâche " $STendTask | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+]  Fin de la tâche "
+}else{
+    Write-Host "[-]  Fin de la tâche "
+}
+
+####################  -- Process creation -- #################### 
+Write-Host "Process creation ..."
+(echo "# Process creation #"| .\nc.exe $ipServ $portServ -w $timer)
+
+# Audit process Creation
+$Error.Clear()
+$AuditProcessCreate=Get-EventLog Security -InstanceId 4688
+if(!$Error){
+    (echo "#Audit process Creation " $AuditProcessCreate | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+]  Audit process Creation "
+}else{
+    Write-Host "[-]  Audit process Creation "
+}
+
+# Audit process Terminaison
+$Error.Clear()
+$AuditProcessTerm=Get-EventLog Security -InstanceId 4689
+if(!$Error){
+    (echo "#Audit process Terminaison " $AuditProcessTerm | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+]  Audit process Terminaison "
+}else{
+    Write-Host "[-]  Audit process Terminaison "
+}
+
+####################  -- PowerShell -- #################### 
+Write-Host "PowerShell ..."
+(echo "# PowerShell #"| .\nc.exe $ipServ $portServ -w $timer)
+
+# Execution de commande
+$Error.Clear()
+$PSexecCommand=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-PowerShell/Operational";ID="4103"}
+if(!$Error){
+    (echo "#Execution de commande " $PSexecCommand | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+]  Execution de commande "
+}else{
+    Write-Host "[-]  Execution de commande "
+}
+
+# Script Block Logging
+$Error.Clear()
+$PSblockLog=Get-WinEvent -FilterHashtable @{logname="Microsoft-Windows-PowerShell/Operational";ID="4104"}
+if(!$Error){
+    (echo "#Script Block Logging " $PSblockLog | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+] Script Block Logging "
+}else{
+    Write-Host "[-] Script Block Logging "
+}
+
+# Pipe Line Execution détail
+$Error.Clear()
+$PSpipeLine=Get-WinEvent -FilterHashtable @{logname="Windows PowerShell";ID="800"}
+if(!$Error){
+    (echo "# Pipe Line Execution détail " $PSpipeLine | .\nc.exe $ipServ $portServ -w $timer)
+    Write-Host "[+]  Pipe Line Execution détail "
+}else{
+    Write-Host "[-]  Pipe Line Execution détail "
+}
+
+ Write-Host "#### Fin du script #### "
